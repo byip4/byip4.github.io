df486d0d23df39bc3567342e66abf6834ee55fe8
Brandon Yip <10019398@sbschools.org>
2016-01-29 10:10:00 -0500

    k

---
+++ js/loadRepeatedDivs.js
@@ -0,0 +1,5 @@
+function loadRepeatedDivs(){
+    $("#navigation-bar").load("js/loadNavBar.html");
+    $("#footer-bar").load("js/loadFooterBar.html");
+}
+jQuery(document).ready(function() {loadRepeatedDivs();});
